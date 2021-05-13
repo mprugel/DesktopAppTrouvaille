eb6b8c64d5ef9582ee61c0616dda60143e012cc4
Karparetzu <juli.a98@LIVE.DE>
2021-05-13 18:21:37 +0200

    API angefangen

--- DesktopAppTrouvaille/Program.cs
+++ DesktopAppTrouvaille/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using APIconnector;
 
 namespace DesktopAppTrouvaille
 {
@@ -14,9 +15,11 @@ namespace DesktopAppTrouvaille
         [STAThread]
         static void Main()
         {
+            APIconnection.InitializeClient();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
